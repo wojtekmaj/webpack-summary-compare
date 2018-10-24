74e59c4d61a96a109541e8cf243b3f173a9b215b
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-10-24 10:21:11 +0200

    Update debug data to include hashes

--- src/__debug/getDebugValue.js
+++ src/__debug/getDebugValue.js
@@ -4,23 +4,23 @@ function* getDebugValue() {
 Version: webpack 4.20.2
 Time: 10000ms
 Built at: 19.10.2018 13:24:53
-            Asset     Size   Chunks             Chunk Names
-another.bundle.js    1 MiB  another  [emitted]  another
-removed.bundle.js  220 KiB  another  [emitted]  another
-  index.bundle.js  550 KiB    index  [emitted]  index
-  child.bundle.js  141 KiB    index  [emitted]  index
+                                 Asset     Size   Chunks             Chunk Names
+another.bundle-2f168e1f7de11a759d50.js    1 MiB  another  [emitted]  another
+removed.bundle-2f168e1f7de11a759d50.js  220 KiB  another  [emitted]  another
+  index.bundle-2f168e1f7de11a759d50.js  550 KiB    index  [emitted]  index
+  child.bundle-2f168e1f7de11a759d50.js  141 KiB    index  [emitted]  index
 Entrypoint index = index.bundle.js
 Entrypoint another = another.bundle.js`;
 
-    yield `Hash: 73ef49f087689d980c7e
+    yield `Hash: c926516bc19d8d5e9e4a
 Version: webpack 4.20.2
 Time: 10000ms
 Built at: 19.10.2018 13:24:53
-            Asset      Size   Chunks             Chunk Names
-another.bundle.js   440 KiB  another  [emitted]  another
- newone.bundle.js  90.1 KiB  another  [emitted]  another
-  index.bundle.js   550 KiB    index  [emitted]  index
-  child.bundle.js   140 KiB    index  [emitted]  index
+                                 Asset      Size   Chunks             Chunk Names
+another.bundle-c926516bc19d8d5e9e4a.js   440 KiB  another  [emitted]  another
+ newone.bundle-c926516bc19d8d5e9e4a.js  90.1 KiB  another  [emitted]  another
+  index.bundle-c926516bc19d8d5e9e4a.js   550 KiB    index  [emitted]  index
+  child.bundle-c926516bc19d8d5e9e4a.js   140 KiB    index  [emitted]  index
 Entrypoint index = index.bundle.js
 Entrypoint another = another.bundle.js`;
   }
