e11494c243db6df418e0c55d7e7aeaaefb484c81
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-12-04 14:10:46 +0100

    Split Input and DragAndDrop component, prevent from dragging multiple files

---
+++ src/DragAndDrop.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import mergeClassNames from 'merge-class-names';
+
+import './DragAndDrop.less';
+
+const readFileAsText = (file) => {
+  const fileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    fileReader.onerror = (error) => {
+      fileReader.abort();
+      reject(error);
+    };
+
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+
+    fileReader.readAsText(file);
+  });
+};
+
+export default class DragAndDrop extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    onChange: PropTypes.func,
+    onlyAccept: PropTypes.number,
+  };
+
+  state = {
+    isActive: false,
+  };
+
+  onDragOver = (event) => {
+    if (!this.shouldReact(event)) {
+      console.log('Skipping');
+      return;
+    }
+
+    event.preventDefault();
+
+    this.setState({
+      isActive: true,
+    });
+  };
+
+  onDrop = (event) => {
+    if (!this.shouldReact(event)) {
+      return;
+    }
+
+    // Prevent default behavior (Prevent file from being opened)
+    event.preventDefault();
+
+    const files = (() => {
+      if (event.dataTransfer.items) {
+        return Array.from(event.dataTransfer.items)
+          .filter(item => item.kind === 'file')
+          .map(item => item.getAsFile());
+      }
+
+      return Array.from(event.dataTransfer.files);
+    })();
+
+    if (files.length === 0) {
+      return;
+    }
+
+    const { onChange } = this.props;
+
+    Promise.all(files.map(readFileAsText)).then(onChange);
+
+    this.cleanupDrag(event);
+  };
+
+    cleanupDrag = (event) => {
+      if (event.dataTransfer.items) {
+        // Use DataTransferItemList interface to remove the drag data
+        event.dataTransfer.items.clear();
+      } else {
+        // Use DataTransfer interface to remove the drag data
+        event.dataTransfer.clearData();
+      }
+
+      this.onDragLeave();
+    }
+
+    onDragLeave = () => {
+      this.setState({
+        isActive: false,
+      });
+    }
+
+    shouldReact(event) {
+      const { onlyAccept } = this.props;
+
+      if (onlyAccept && event.dataTransfer.items.length !== onlyAccept) {
+        return false;
+      }
+
+      return true;
+    }
+
+    render() {
+      const { children } = this.props;
+      const { isActive } = this.state;
+
+      return (
+        <div
+          className={mergeClassNames('DragAndDrop', isActive && 'DragAndDrop--active')}
+          onDragOver={this.onDragOver}
+          onDragLeave={this.onDragLeave}
+          onDrop={this.onDrop}
+        >
+          {
+          React.cloneElement(React.Children.only(children))
+        }
+        </div>
+      );
+    }
+}
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import mergeClassNames from 'merge-class-names';
 
 import './Input.less';
 
 import getDebugValue from './__debug/getDebugValue';
+import DragAndDrop from './DragAndDrop';
 
 const setRows = (el) => {
   if (!el) {
@@ -18,33 +18,16 @@ const setRows = (el) => {
   el.rows = lines;
 };
 
-const readFileAsText = (file) => {
-  const fileReader = new FileReader();
-
-  return new Promise((resolve, reject) => {
-    fileReader.onerror = (error) => {
-      fileReader.abort();
-      reject(error);
-    };
-
-    fileReader.onload = () => {
-      resolve(fileReader.result);
-    };
-
-    fileReader.readAsText(file);
-  });
-};
-
 export default class Input extends Component {
   static propTypes = {
     id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     onChange: PropTypes.func,
-  }
+  };
 
   state = {
     value: getDebugValue.next().value,
-  }
+  };
 
   componentDidMount() {
     this.onTableUpdate();
@@ -54,6 +37,10 @@ export default class Input extends Component {
     const { value } = event.target;
 
     this.setState({ value }, this.onTableUpdate);
+  }
+
+  onDnDChange = ([firstValue]) => {
+    this.setState({ value: firstValue }, this.onTableUpdate);
   }
 
   onTableUpdate = () => {
@@ -67,87 +54,34 @@ export default class Input extends Component {
     }
   }
 
-  onDragOver = (event) => {
-    event.preventDefault();
-
-    this.setState({
-      dragOverActive: true,
-    });
-  };
-
-  onDrop = (event) => {
-    // Prevent default behavior (Prevent file from being opened)
-    event.preventDefault();
-
-    const files = (() => {
-      if (event.dataTransfer.items) {
-        return Array.from(event.dataTransfer.items)
-          .filter(item => item.kind === 'file')
-          .map(item => item.getAsFile());
-      }
-
-      return Array.from(event.dataTransfer.files);
-    })();
-
-    if (files.length === 0) {
-      return;
-    }
-
-    const [firstFile] = files;
-
-    readFileAsText(firstFile).then((value) => {
-      this.setState({ value }, this.onTableUpdate);
-    });
-
-    this.cleanupDrag(event);
-  };
-
-  cleanupDrag = (event) => {
-    if (event.dataTransfer.items) {
-      // Use DataTransferItemList interface to remove the drag data
-      event.dataTransfer.items.clear();
-    } else {
-      // Use DataTransfer interface to remove the drag data
-      event.dataTransfer.clearData();
-    }
-
-    this.onDragLeave();
-  }
-
-  onDragLeave = () => {
-    this.setState({
-      dragOverActive: false,
-    });
-  }
-
   render() {
     const { id, label } = this.props;
-    const { dragOverActive, value } = this.state;
+    const { value } = this.state;
 
     return (
-      <div
-        className={mergeClassNames('Input', dragOverActive && 'Input--dragOverActive')}
-        onDragOver={this.onDragOver}
-        onDragLeave={this.onDragLeave}
-        onDrop={this.onDrop}
+      <DragAndDrop
+        onlyAccept={1}
+        onChange={this.onDnDChange}
       >
-        <h3>
-          <label htmlFor={id}>{label}</label>
-        </h3>
-        <textarea
-          id={id}
-          value={value}
-          onChange={this.onChange}
-          ref={(ref) => {
-            if (ref) {
-              setRows(ref);
-            }
+        <div className="Input">
+          <h3>
+            <label htmlFor={id}>{label}</label>
+          </h3>
+          <textarea
+            id={id}
+            value={value}
+            onChange={this.onChange}
+            ref={(ref) => {
+              if (ref) {
+                setRows(ref);
+              }
 
-            this.textarea = ref;
-          }}
-          wrap="off"
-        />
-      </div>
+              this.textarea = ref;
+            }}
+            wrap="off"
+          />
+        </div>
+      </DragAndDrop>
     );
   }
 }
