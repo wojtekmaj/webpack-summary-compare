465e4097c0511a7ff60f573f5f76d5a1c36d2636
Wojciech Maj <w.maj@intive.com>
2019-02-22 14:57:40 +0100

    Add source map to debug output

--- src/__debug/getDebugValue.js
+++ src/__debug/getDebugValue.js
@@ -4,11 +4,12 @@ function* getDebugValue() {
 Version: webpack 4.20.2
 Time: 10000ms
 Built at: 19.10.2018 13:24:53
-                                 Asset     Size   Chunks             Chunk Names
-another.bundle-2f168e1f7de11a759d50.js    1 MiB  another  [emitted]  another
-removed.bundle-2f168e1f7de11a759d50.js  220 KiB  another  [emitted]  another
-  index.bundle-2f168e1f7de11a759d50.js  550 KiB    index  [emitted]  index
-  child.bundle-2f168e1f7de11a759d50.js  141 KiB    index  [emitted]  index
+                                     Asset     Size   Chunks             Chunk Names
+    another.bundle-2f168e1f7de11a759d50.js    1 MiB  another  [emitted]  another
+    removed.bundle-2f168e1f7de11a759d50.js  220 KiB  another  [emitted]  another
+removed.bundle-2f168e1f7de11a759d50.js.map  220 KiB  another  [emitted]  another
+      index.bundle-2f168e1f7de11a759d50.js  550 KiB    index  [emitted]  index
+      child.bundle-2f168e1f7de11a759d50.js  141 KiB    index  [emitted]  index
 Entrypoint index = index.bundle.js
 Entrypoint another = another.bundle.js`;
 
@@ -16,11 +17,12 @@ Entrypoint another = another.bundle.js`;
 Version: webpack 4.20.2
 Time: 9870ms
 Built at: 19.10.2018 13:24:53
-                                 Asset      Size   Chunks             Chunk Names
-another.bundle-c926516bc19d8d5e9e4a.js   440 KiB  another  [emitted]  another
- newone.bundle-c926516bc19d8d5e9e4a.js  90.1 KiB  another  [emitted]  another
-  index.bundle-c926516bc19d8d5e9e4a.js   550 KiB    index  [emitted]  index
-  child.bundle-c926516bc19d8d5e9e4a.js   140 KiB    index  [emitted]  index
+                                     Asset      Size   Chunks             Chunk Names
+    another.bundle-c926516bc19d8d5e9e4a.js   440 KiB  another  [emitted]  another
+     newone.bundle-c926516bc19d8d5e9e4a.js  90.1 KiB  another  [emitted]  another
+ newone.bundle-c926516bc19d8d5e9e4a.js.map  90.1 KiB  another  [emitted]  another
+      index.bundle-c926516bc19d8d5e9e4a.js   550 KiB    index  [emitted]  index
+      child.bundle-c926516bc19d8d5e9e4a.js   140 KiB    index  [emitted]  index
 Entrypoint index = index.bundle.js
 Entrypoint another = another.bundle.js`;
   }
